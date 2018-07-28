eac67b7b596339aba0dc23f709c68e76a07c19f9
John Minter <jrminter@gmail.com>
2018-07-28 15:41:28 -0400

    added penepma_get_intensities function

---
+++ R/penepma_get_intensities.R
@@ -0,0 +1,26 @@
+#' Retrieve the intensities from a PENEPMA spectrum
+#'
+#' @param fi String. The path to the file. ex: "./pe-spect-01.dat"
+#'
+#' @return the intensities (a tibble)
+#' 
+#' @import dplyr
+#' 
+#' @export
+#' 
+penepma_get_intensities <- function(fi){
+  x <- readLines(fi, 13)[13]
+  y <- strsplit(x, " ") #[[1]][7])
+  y <- unlist(lapply(y, function(x) x[nchar(x) > 0]))[-1]
+  y <- y[-4]
+  df <- read.table(fi, sep = "" , header = F , skip=14)
+  names(df) <- y
+  names(df)[4] <- 'eV'
+  df$eV <- df$eV/1000.
+  # df$IZ <- as.integer(df$IZ)
+  # names(df)[4] <- 'keV'
+  names(df) <- c("IZ", "S0", "S1", "keV", "P.mu", "P.se", "C.mu", "C.se",
+                 "B.mu", "B.se", "TF.mu", "TF.se", "Int.mu", "Int.se")
+  df <- as_tibble(df)
+  return(df)
+}
