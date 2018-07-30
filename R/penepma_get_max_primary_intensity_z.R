571dc4c97cd4f7a9c5148594c31cf2ccda4bd5c7
John Minter <jrminter@gmail.com>
2018-07-30 11:23:38 -0400

    V 0.9007. Added computation of the SNR from the max primary intensity and max total intensity for an element.

--- R/penepma_get_max_primary_intensity_z.R
+++ R/penepma_get_max_primary_intensity_z.R
@@ -1,10 +1,11 @@
 #' Retrieve the maximum primary intensity and uncertainty for an element
+#' and calculate the SNR
 #'
 #' @param tib A tibble. The tibble with the intensities from a penepma spectrum
 #' 
 #' @param z The atomic number for an element
 #'
-#' @return the transition and both the maximum primary intensity and uncertainty (a tibble)
+#' @return A tibble. The transition, maximum primary intensity, uncertainty, and SNR.
 #' 
 #' @import dplyr
 #' 
@@ -14,7 +15,8 @@ penepma_get_max_primary_intensity_z <- function(tib, z){
   rv <- tib %>%
     filter(IZ==z) %>%
     filter(P.mu == max(P.mu)) %>%
-    select(IZ, S0, S1, P.mu, P.se)
+    select(IZ, S0, S1, P.mu, P.se) %>%
+    mutate(P.snr=P.mu/P.se)
   
   return(rv)
 }
--- R/penepma_get_max_total_intensity_z.R
+++ R/penepma_get_max_total_intensity_z.R
@@ -1,10 +1,11 @@
 #' Retrieve the maximum total intensity qnd uncertqainty for an element
+#' and calculate the SNR.
 #'
 #' @param tib A tibble. The tibble with the intensities from a penepma spectrum
 #' 
 #' @param z The atomic number for an element
 #'
-#' @return the transition and the maximum total intensity and uncertainty (a tibble)
+#' @return A tibble. The transition, maximum total intensity, uncertainty, and SNR.
 #' 
 #' @import dplyr
 #' 
@@ -14,7 +15,8 @@ penepma_get_max_total_intensity_z <- function(tib, z){
   rv <- tib %>%
     filter(IZ==z) %>%
     filter(Int.mu == max(Int.mu)) %>%
-    select(IZ, S0, S1, Int.mu, Int.se)
+    select(IZ, S0, S1, Int.mu, Int.se) %>%
+    mutate(Int.snr=Int.mu/Int.se)
   
   return(rv)
 }
