e91f0ad5c8bf729e264505a7546b066fc893387f
John Minter <jrminter@gmail.com>
2018-08-01 10:27:21 -0400

    added elemental_densities data set

---
+++ R/elemental_densities_data.R
@@ -0,0 +1,13 @@
+#' @name elemental_densities
+#' @aliases elemental_densities
+#' @title Densities for the elements.
+#' @description A dataframe/tibble containing the Density, Unit,
+#'  Name, Symbol, and Z for 96 element.
+#' @docType data
+#' @usage data(elemental_densities)
+#' @format A dataframe/tibble with 96 observations of the variables
+#'  \code{Density}, \code{Unit}, \code{Name}, \code{Symbol} and \code{Z}
+#   1 row element.
+#' @source https://www.angstromsciences.com/density-elements-chart
+#' @author angstromsciences
+NULL
