a9aaa2ffa26b52ba9cd8716d3635497f377802f7
John Minter <jrminter@gmail.com>
2018-08-04 21:47:33 -0400

    mod penepma_read_raw_data.R

--- R/penepma_read_raw_data.R
+++ R/penepma_read_raw_data.R
@@ -1,16 +1,24 @@
 #' Retrieve the raw channel data from a PENEPMA spectrum
 #'
 #' @param fi String. The path to the file. ex: "./pe-spect-01.dat"
-#'
-#' @return a data frame
+#' 
+#' @param min_intensity_clip numeric. The minimum value to clip the intensity. Default: 1.0e-12.
+#' 
+#' @param int_scale_factor numeric. Intensity scale factor. Default: 1.0e9.
+#' 
+#' @return a tibble
 #' 
 #' @import ramify
+#' 
+#' @import dplyr
 #' 
 #' @importFrom utils read.table
 #'
 #' @export
 #'
-penepma_read_raw_data <- function(fi){
+penepma_read_raw_data <- function(fi,
+                                  min_intensity_clip=1.0e-12,
+                                  int_scale_factor=1.0e9){
   df <- read.table(fi, sep=" ", skip=12)
   df <- df[, -c(1,2,3) ]
   df <- df[, -c(2, 4)]
@@ -19,10 +27,10 @@ penepma_read_raw_data <- function(fi){
   names(df) <- c('keV', 'mu', 'se') # make keV
 
   mv <- max(df$mu)
-  df$mu <- clip(df$mu, .min = 1.0e-12, .max = mv)
+  df$mu <- clip(df$mu, .min = min_intensity_clip, .max = mv)
   df$mu <- 1e9*df$mu
   mv <- max(df$se)
-  df$se <- clip(df$se, .min = 1.0e-12, .max = mv)
+  df$se <- clip(df$se, .min = min_intensity_clip, .max = mv)
   df$se <- 1e9*df$se
-  return(df)
+  return(as_tibble(df))
 }
