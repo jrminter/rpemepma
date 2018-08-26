d293b1fcd5098a077c1d270c64dafff3cc66f122
John Minter <jrminter@gmail.com>
2018-08-26 19:15:49 -0400

    added add'l params to penepma16_get_time_traj_uncert

--- R/penepma16_get_time_traj_uncert.R
+++ R/penepma16_get_time_traj_uncert.R
@@ -1,7 +1,16 @@
 #' Get the time, number of trajectories, and relative uncertainty from a
-#' penepma16 simulation
+#' penepma16 simulation. Sometimes the lines in the file are shifted.
 #'
 #' @param sim_dir The path to the directory with penepma.res file.
+#' 
+#' @param skip_time An integer. The number of lines to skip to read the line
+#' containing the simulation time. Default:6.
+#' 
+#' @param skip_traj An integer. The number of lines to skip to read the line
+#' containing the number of trajectories, Default: 2 (additional lines).
+#' 
+#' @param skip_unc An integer. The number of lines to skip to read the line
+#' with the uncertainty value. Default: 33 (additional lines).
 #' 
 #' @return a tibble with the simulation time (sec), number of
 #' trajectories, and the relative uncertainty of the REFLIN transition
@@ -10,14 +19,20 @@
 #' 
 #' @export
 #' 
-penepma16_get_time_traj_uncert <- function(sim_dir){
+penepma16_get_time_traj_uncert <- function(sim_dir,
+                                          skip_time = 6,
+                                          skip_traj = 2,
+                                          skip_unc = 33){
     path <- sprintf("%s/penepma-res.dat", sim_dir)
     print(path)
     con <- file(path)
     open(con)
-    line_time <- read.table(con, skip =  6, nrow=1)[4] #  7th line
-    traj      <- as.double(read.table(con, skip =  1, nrow=1)[5]) # 10th line
-    rel_unc   <- read.table(con, skip = 41, nrow=1)[4] # 52nd line
+    # 7th line
+    line_time <- read.table(con, skip = skip_time, nrow=1)[4] 
+    # 10th line
+    traj      <- as.double(read.table(con, skip = skip_traj, nrow=1)[5]) 
+    # 44th line
+    rel_unc   <- read.table(con, skip = skip_unc, nrow=1)[4] 
     close(con)
     df <- data.frame(sim_time = line_time,
                      num_traj = traj,
