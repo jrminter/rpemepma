e06e106df8d2a04e70e301dad90a114131137a3a
John Minter <jrminter@gmail.com>
2018-09-04 14:24:45 -0400

    bringing up to date on this system

---
+++ R/cm_to_nm.R
@@ -0,0 +1,16 @@
+#' Convert cm to nm for processing PENEPMA data
+#'
+#' @param nm A number. The value to convert.
+#'
+#' @return cm The result in cm 
+#'                   
+#' @export
+#'
+#' @examples
+#' library(rpenepma)
+#' print(cm_to_nm(20.0))
+
+cm_to_nm <- function(cm){
+    nm <- 1.0e7*cm
+    return(nm)
+}
---
+++ R/nm_to_cm.R
@@ -0,0 +1,17 @@
+#' Convert nm to cm for processing PENEPMA data
+#'
+#' @param cm A number. The value to convert.
+#'
+#' @return cm The result in cm 
+#'                   
+#' @export
+#'
+#' @examples
+#' library(rpenepma)
+#' print(cm_to_nm(2.0e-6))
+
+nm_to_cm <- function(nm){
+    cm <- 1.0e-7*nm
+    return(cm)
+}
+
